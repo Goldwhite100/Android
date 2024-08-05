468a72d62f8da78ec8418513fb9c1dd47f9a6e8a
Goldwhite100 <aokoh365h@gmail.com>
2024-08-05 09:50:43 +0300

    Android

--- app/src/main/java/com/example/propertyplus/ui/theme/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/propertyplus/ui/theme/screens/home/HomeScreen.kt
@@ -1,31 +1,51 @@
 package com.example.propertyplus.ui.theme.screens.home
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.propertyplus.R
+import com.example.propertyplus.ui.theme.newGreen
 
 
 @Composable
 fun HomeScreen(navController: NavController){
 
     Column(
-        modifier = Modifier.fillMaxSize() . background(Color.Gray),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(newGreen),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.home) ,
+            contentDescription = "home",
+            modifier = Modifier.size(300.dp).clip(shape = CircleShape),
+            contentScale = ContentScale.Crop
+
+        )
+
         Text(
             text = "PropertyPlus",
             fontSize = 60.sp,
